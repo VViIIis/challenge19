315c022599bbf042381d357eb13228422fe03300
VViIIis <jtwillis113@gmail.com>
2022-08-11 13:06:07 -0500

    final commit

--- fintech_finder.py
+++ fintech_finder.py
@@ -54,9 +54,7 @@ w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 # When the information has been added, rename the file `.env`.
 #from mnemonic import Mnemonic
 
-#load_dotenv()
-#mnemonic = os.getenv("MNEMONIC")
-#type(mnemonic)
+
 import os
 import requests
 from dotenv import load_dotenv
@@ -69,137 +67,18 @@ from web3.gas_strategies.time_based import medium_gas_price_strategy
 # * `generate_account`
 # * `get_balance`
 # * `send_transaction`
-def generate_account():
-    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
-    # Fetch mnemonic from environment variable.
-    mnemonic = os.getenv("MNEMONIC")
-
-    # Create Wallet Object
-    wallet = Wallet(mnemonic)
-
-    # Derive Ethereum Private Key
-    private, public = wallet.derive_account("eth")
-
-    # Convert private key into an Ethereum account
-    account = Account.privateKeyToAccount(private)
-
-    return account
-
-def get_balance(w3, address):
-    """Using an Ethereum account address access the balance of Ether"""
-    # Get balance of address in Wei
-    wei_balance = w3.eth.get_balance(address)
-
-    # Convert Wei value to ether
-    ether = w3.fromWei(wei_balance, "ether")
-
-    # Return the value in ether
-    return ether
-
-
-def send_transaction(w3, account, to, wage):
-    """Send an authorized transaction to the Ganache blockchain."""
-    # Set gas price strategy
-    w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
-
-    # Convert eth amount to Wei
-    value = w3.toWei(wage, "ether")
-
-    # Calculate gas estimate
-    gasEstimate = w3.eth.estimateGas({"to": to, "from": account.address, "value": value})
-
-    # Construct a raw transaction
-    raw_tx = {
-        "to": to,
-        "from": account.address,
-        "value": value,
-        "gas": gasEstimate,
-        "gasPrice": 0,
-        "nonce": w3.eth.getTransactionCount(account.address)
-    }
-
-    # Sign the raw transaction with ethereum account
-    signed_tx = account.signTransaction(raw_tx)
-
-    # Send the signed transactions
-    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-
-
+from crypto_wallet import generate_account, get_balance, send_transaction
 # 4. Within the Streamlit sidebar section of code, create a variable named
 # `account`. Set this variable equal to a call on the `generate_account`
 # function. This function will create the Fintech Finder customer’s (in this
 # case, your) HD wallet and Ethereum account.
-account = generate_account()
+
 # 5. Within this same section of the `fintech_finder.py` file, define a
 # new `st.sidebar.write` function that will display the balance of the
 # customer’s account. Inside this function, call the `get_balance` function
 # and pass it your Ethereum `account.address`.
-st.sidebar.write(get_balance)
-
-################################################################################
-# Step 1 - Part 3:
-# Import the following functions from the `crypto_wallet.py` file:
-# * `generate_account`
-# * `get_balance`
-# * `send_transaction`
-
-# @TODO:
-# From `crypto_wallet.py import the functions generate_account, get_balance,
-#  and send_transaction
-def generate_account():
-    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
-    # Fetch mnemonic from environment variable.
-    mnemonic = os.getenv("MNEMONIC")
-
-    # Create Wallet Object
-    wallet = Wallet(mnemonic)
-
-    # Derive Ethereum Private Key
-    private, public = wallet.derive_account("eth")
-
-    # Convert private key into an Ethereum account
-    account = Account.privateKeyToAccount(private)
-
-    return account
-
-def get_balance(w3, address):
-    """Using an Ethereum account address access the balance of Ether"""
-    # Get balance of address in Wei
-    wei_balance = w3.eth.get_balance(address)
-
-    # Convert Wei value to ether
-    ether = w3.fromWei(wei_balance, "ether")
-
-    # Return the value in ether
-    return ether
 
 
-def send_transaction(w3, account, to, wage):
-    """Send an authorized transaction to the Ganache blockchain."""
-    # Set gas price strategy
-    w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
-
-    # Convert eth amount to Wei
-    value = w3.toWei(wage, "ether")
-
-    # Calculate gas estimate
-    gasEstimate = w3.eth.estimateGas({"to": to, "from": account.address, "value": value})
-
-    # Construct a raw transaction
-    raw_tx = {
-        "to": to,
-        "from": account.address,
-        "value": value,
-        "gas": gasEstimate,
-        "gasPrice": 0,
-        "nonce": w3.eth.getTransactionCount(account.address)
-    }
-
-    # Sign the raw transaction with ethereum account
-    signed_tx = account.signTransaction(raw_tx)
-
-    # Send the signed transactions
-    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
 ################################################################################
 # Fintech Finder Candidate Information
@@ -250,7 +129,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-account = generate_account(w3)
+account = generate_account()
 
 ##########################################
 
@@ -266,7 +145,7 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-st.sidebar.write(get_balance)
+st.sidebar.write(get_balance(w3, account.address))
 ##########################################
 
 # Create a select box to chose a FinTech Hire candidate
@@ -399,7 +278,7 @@ if st.sidebar.button("Send Transaction"):
 
 # The function that starts the Streamlit application
 # Writes FinTech Finder candidates to the Streamlit page
-get_people()
+get_people(w3)
 
 ################################################################################
 # Step 3: Inspect the Transaction
